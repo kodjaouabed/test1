ea47cfcf7ca220dd6b753bf785e4878e13190352
kodjaouabed <bossor755@gmail.com>
2024-09-14 15:42:33 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Abed</p>
-      </header>
+    <div class="containeur_chargement">
+      <div class="ring"></div>
+      <div class="ring"></div>
+      <div class="ring"></div>
+      <span>
+      <img src="devkan.png" alt="" style={{width:200,height:200}} />
+      </span>
     </div>
   );
 }
